681e0addbbf6b4ed577d17246ed504b8b0dc2dba
KEDIARAHUL135 <kediarahul135@gmail.com>
2021-03-23 14:13:01 +0530

    Add code to find box coordinates
    The code first finds the aruco markers in the frame and then finds the
    coordinates of the box on which the projection video will be projected.

--- main.py
+++ main.py
@@ -4,12 +4,33 @@ import numpy as np
 
 
 
+def FindBoxCoordinates(Frame):
+    # Detecting markerrs
+    GrayFrame = cv2.cvtColor(Frame,cv2.COLOR_BGR2GRAY)
+    ArucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_50)
+    Parameters = cv2.aruco.DetectorParameters_create()
+    Corners, IDs, RejectedImgPoints = cv2.aruco.detectMarkers(GrayFrame, ArucoDict, parameters=Parameters)
+
+    # If no aruco marker found or number of aruco merkers found is not 4, return None
+    if IDs is None:
+        return None
+    if len(IDs) != 4:
+        return None
+
+    # Storing Box coordinates
+    BoxCoordinates = []
+    for i in range(4):
+        BoxCoordinates.append(Corners[int(np.where(IDs == i)[0])][0][i])
+
+    return BoxCoordinates    
+
+
 if __name__ == "__main__":
     # Firstly, reading aruco video.
-    ArucoVid_Cap = cv2.VideoCapture("Videos/ArucoVideo.avi")
+    ArucoVid_Cap = cv2.VideoCapture("Videos/ArucoVideo_OnWall.mp4")
 
     # Reading video for projection
-    ProjVid_Cap = cv2.VideoCapture("Videos/ProjVid1.mpeg")
+    ProjVid_Cap = cv2.VideoCapture("Videos/ProjVid.mpeg")
 
     # Creating video writer object
     OutVid = cv2.VideoWriter('Videos/FinalVideo.avi', cv2.VideoWriter_fourcc(*'XVID'), ArucoVid_Cap.get(cv2.CAP_PROP_FPS), (int(ArucoVid_Cap.get(3)), int(ArucoVid_Cap.get(4)))) 
@@ -24,4 +45,21 @@ if __name__ == "__main__":
             print("Not able to read projection video.")
             exit()
         
-        exit()
+        # Reading frames
+        retArucoVid, ArucoVid_Frame = ArucoVid_Cap.read()
+        retProjVid, ProjVid_Frame = ProjVid_Cap.read()
+
+        # End the output video if retArucoVid is false (video finished)
+        if not retArucoVid:
+            OutVid.release()
+            print("Stimulation Completed.")
+            exit()
+
+        # Restart the projection video if finished
+        if not retProjVid:
+            ProjVid_Cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            retProjVid, ProjVid_Frame = ProjVid_Cap.read()
+
+        
+        # Detecting Aruco markers in the frame
+        BoxCoordinates = FindBoxCoordinates(ArucoVid_Frame)
